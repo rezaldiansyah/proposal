ceb8c0f7a063153d3aa4bb4463165fccef7cfa2b
Reza Aldi <rezaaldi@Rezas-MacBook-Pro.local>
2026-01-03 20:29:46 +0700

    Fix: Change base path to / for custom domain support

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/proposal/' : '/',
+    base: '/',
     build: {
         outDir: 'dist',
         rollupOptions: {
