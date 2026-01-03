117dd08ea65915dc5675bfa3f16d6ac76b38038f
Reza Aldi <rezaaldi@Rezas-MacBook-Pro.local>
2026-01-03 20:39:04 +0700

    Add 3 new proposal cards: TRUST, SIMS, AI Customer Services

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,10 @@ export default defineConfig({
                 'habits-media': resolve(__dirname, 'projects/habits-media/index.html'),
                 'annual-meeting': resolve(__dirname, 'presentations/annual-meeting-2026/index.html'),
                 'pinara-digital': resolve(__dirname, 'proposals/pinara-digital/index.html'),
-                'shifr-asia': resolve(__dirname, 'proposals/shifr-asia/index.html')
+                'shifr-asia': resolve(__dirname, 'proposals/shifr-asia/index.html'),
+                'trust': resolve(__dirname, 'proposals/trust/index.html'),
+                'sims': resolve(__dirname, 'proposals/sims/index.html'),
+                'ai-customer-services': resolve(__dirname, 'proposals/ai-customer-services/index.html')
             }
         }
     },
